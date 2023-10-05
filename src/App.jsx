d769aeb375dd2866787138adf7fa56d292089149
Bean <alexanderafoko@gmail.com>
2023-10-05 09:40:14 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./stylesheets/App.scss";
+
+export default function App() {
+  let version = 0.1;
+  return (
+    <div id="app">
+      <h1>CrankiNotes v{version} ✍️</h1>
+    </div>
+  );
+}
